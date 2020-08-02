579bf5bd774b1d59c07b599f4682d83029cc64aa
stphn1117 <stephanie1117@hotmail.es>
2020-08-01 18:52:23 -0600

    comunication client-->server

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cpr/cpr.h>
+//build
+//g++ -std=c++11 -o main -lcpr main.cpp
 int main(){
     std::cout<<"hello world";
     return 0;
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,3 +1,24 @@
+
+const net = require('net');
+
+// Create a server object
+const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  socket.write('SERVER: Hello! This is server speaking.\n');
+  socket.end('SERVER: Closing connection now.\n');
+}).on('error', (err) => {
+  console.error(err);
+});
+
+// Open server on port 3000
+server.listen(3000, () => {
+  console.log('opened server on', server.address().port);
+});
+/*
 const express = require('express');
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
+*/
