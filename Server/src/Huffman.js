4811c81bd67fcf5f5cf7adf12d2dad78f27cc5b7
Valeria <valeriamorales40@gmail.com>
2020-08-04 12:02:51 -0600

    Huffman code advances

--- Server/src/Huffman.js
+++ Server/src/Huffman.js
@@ -1,47 +1,42 @@
-
 var freqMatrix = [];
 
 //freqMatrix = [[frequency, "char"],
 //              [frequency, "char"]]
 
-function readTextFile(file)
-{
+function readTextFile(file) {
     var fs = require('fs');
     var textByLine = fs.readFileSync(file).toString();
     console.log(textByLine);
     return textByLine;
 }
 
-function frecuency(fileString)
-{
+function frecuency(fileString) {
     var freq = [];
-    for (var j = 0; j < fileString.length; j++) 
-    {
+    for (var j = 0; j < fileString.length; j++) {
         var character = fileString.charAt(j);
 
-        if (freq[character]){
+        if (freq[character]) {
             freq[character]++;
 
-        }else {
+        } else {
             freq[character] = 1;
         }
     }
 
-    for(var i in freq)
-    {
+    for (var i in freq) {
         var temp = [];
         temp.push(freq[i]);
         temp.push(i);
         freqMatrix.push(temp);
     }
 
-    freqMatrix.sort(function(a,b){
-        return a[0]-b[0]
+    freqMatrix.sort(function (a, b) {
+        return a[0] - b[0]
     });
 
     console.log(freqMatrix);
 
-} 
+}
 
 var file = readTextFile('Server/src/compress.txt');
 frecuency(file);
