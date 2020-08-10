09a278384884ccbd63f754e5c12c2be7ae70d9c8
josfemova <josfemova@gmail.com>
2020-08-09 19:29:53 -0600

    restructure client

--- Server/src/DataBase.js
+++ Server/src/DataBase.js
@@ -1,12 +1,25 @@
-const mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
-  database : 'got',
-  insecureAuth : true
-});
-connection.end();
-class database{
+let mysql = require("mysql");
 
+class DataBase {
+    static #mysqlInfo = {
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'GOT',
+        insecureAuth: true
+    }
+    static addcommit() {
+
+    }
+    static addfile() {
+
+    }
+    static test() {
+        let connection = mysql.createConnection(this.#mysqlInfo);
+        connection.query("show tables",(err, res)=>{
+            console.log(res);
+        })
+        connection.end();
+    }
 }
+DataBase.test();
