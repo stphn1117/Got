4f6f3ceae25b9d0b7e1f0c51378f537a3a710df1
josfemova <josfemova@gmail.com>
2020-08-13 13:53:33 -0600

    changing to mysql2

--- Server/src/DataBase.js
+++ Server/src/DataBase.js
@@ -1,6 +1,5 @@
-let mysql = require("mysql");
+let mysql2 = require("mysql2/promise");
 let util = require("util");
-const Huffman = new (require("./Huffman").Huffman)();
 const md5 = require("md5");
 const diff = require("diff");
 
@@ -29,10 +28,11 @@ class DataBase {
         return this.instance;
     }
 
-    insertRepo(name, callback) {
+    async insertRepo(name, callback) {
         let sql = `INSERT INTO REPOSITORIO (nombre) VALUES ("${name}")`;
-        console.log(sql)
-        let conn = mysql.createConnection(this.mysql);
+        let conn = mysql2.createConnection(this.mysql);
+        
+
         conn.query(sql, (err, result, fields)=>{
             callback(result.InsertId);
 			conn.end();
@@ -48,11 +48,11 @@ class DataBase {
      * @param {lista de cambios a ser guardados sobre los archivos} changes 
      * @param {función que recibe el id del commit procesado} callback 
      */
-    addCommit(repositorioId, autor, mensaje, hora, addFiles, changes, callback) {
+    async addCommit(repositorioId, autor, mensaje, hora, addFiles, changes, callback) {
         let sql = `INSERT INTO COMMITS (rep_id, autor, mensaje, hora)
                 VALUES (${repositorioId}, ${autor}, ${mensaje}, ${Date.now});`;
 
-        let conn = mysql.createConnection(this.mysql)
+        let conn = mysql2.createConnection(this.mysql)
         conn.query(sql, (err, result, fields)=>{
 
 
@@ -61,39 +61,19 @@ class DataBase {
 			conn.end();
 		});
     }
-	getFile(fileId, callback){
+	async getFile(fileId, callback){
 		let sql = `SELECT * FROM ARCHIVO`;
 		
     }
-    test(callback) {
-        let conn = mysql.createConnection(this.mysql)
-        function queryProcess(err, result, fields){
-            tables = [];
-            result.forEach(x => {tables.push(x.Tables_in_GOT)});
-            conn.end();
-        }
-        conn.query("show tables", queryProcess);
+    async test2(){
+        const conn = await mysql2.createConnection(this.mysql)
+        const [result] = await conn.execute(`SHOW TABLES`);
+        conn.end()
+        return result;
     }
 }
 
 module.exports.DataBase = DataBase;
 
 let DB = DataBase.Instance()
-console.log(DB.insertRepo("hemlo",(a)=>{
-    console.log(a);
-}))
-//select archivo test
 
-console.log(DB.getFile(1,(file)=>{
-	console.log(file);
-}));
-
-
-/*
-tablaCodigos = [
-    {"codigo":01,"simbolo":"a"},
-    {"codigo":2123123,"simbolo":"w"},
-    {"codigo":2123123,"simbolo":"w"},
-    {"codigo":2123123,"simbolo":"w"}
-]
-codigo=[1,2,3,4,123,10111,122210101,010101];*/
