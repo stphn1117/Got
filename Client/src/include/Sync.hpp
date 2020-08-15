27f55f389369fe54e6980ea41db5515033efc9b6
Valeria <valeriamorales40@gmail.com>
2020-08-15 00:39:31 -0600

    Bug fixing in merge

--- Client/src/include/Sync.hpp
+++ Client/src/include/Sync.hpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 using namespace std;
-
+/**
+ * @brief Clase para la sincronizacion de archivos
+ * 
+ */
 class Sync{
     public:
     string finalContent = "";
     string buffer = "";
-    
     void merge(string localFilePath, string remoteFile);
     int askUser(string localFilePath);
 };
